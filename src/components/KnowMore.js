49302d2a4896ba09d6bdc027bd0435f8bc523993
anurag <anurag@xyrocodes.com>
2023-04-22 22:34:03 +0530

    first commit

--- src/components/KnowMore.js
+++ src/components/KnowMore.js
@@ -36,11 +36,9 @@ class KnowMore extends React.Component {
                     <p>5. To navigate between a menu items you need to rotate on track wheel</p>
                     <p>6. To go to next menu or go inside a menu press center button and to go to previous menu press menu button</p>
                     <p>7. Songs do play, Please checkout settings menu</p>
-                    <p>Github-Link : <a href="https://github.com/RitishKhanna/iPod-Classic">iPod</a> 
+                    <p>Hosting-Link : <a href="">iPod-Hosted</a> 
                     </p>
-                    <p>Hosting-Link : <a href="http://RitishKhanna.github.io/iPod-Classic">iPod-Hosted</a> 
-                    </p>
-                    <p>App Developed by : <a href="https://github.com/RitishKhanna">Ritish Khanna</a></p>
+                    <p>App Developed by : <a href="https://github.com/xyrocodes">Anurag Ranjan</a></p>
                     <p>credits : Apple, Flaticon</p>
                 </div>
                 <button id="info-btn" onClick={this.openDiv}>Know More</button>
