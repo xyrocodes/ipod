6310ddae67b54ea3006b64e2b619bd9b860dd1f3
Anurag <89177393+xyrocodes@users.noreply.github.com>
2023-04-22 23:58:02 +0530

    Update KnowMore.js

--- src/components/KnowMore.js
+++ src/components/KnowMore.js
@@ -36,7 +36,7 @@ class KnowMore extends React.Component {
                     <p>5. To navigate between a menu items you need to rotate on track wheel</p>
                     <p>6. To go to next menu or go inside a menu press center button and to go to previous menu press menu button</p>
                     <p>7. Songs do play, Please checkout settings menu</p>
-                    <p>Hosting-Link : <a href="">iPod-Hosted</a> 
+                    <p>Hosting-Link : <a href="https://xyrocodes.github.io/ipod/">iPod-Hosted</a> 
                     </p>
                     <p>App Developed by : <a href="https://github.com/xyrocodes">Anurag Ranjan</a></p>
                     <p>credits : Apple, Flaticon</p>
